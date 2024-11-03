759a24d35e1014bb8f67b55a83378621304189b1
drew2799 <andreacrespi99@gmail.com>
2024-11-03 14:55:26 -0500

    Updating time and memory requirements

--- src/PATHinit_job.sh
+++ src/PATHinit_job.sh
@@ -3,13 +3,13 @@
 #SBATCH --account=rrg-wperciva
 #SBATCH --job-name=PATHinit
 #SBATCH --output=PATHinit.out
-#SBATCH --time=10:00
+#SBATCH --time=1:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --mem-per-cpu=5G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=50G
 #SBATCH --mail-user=a2crespi@uwaterloo.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 module load julia/1.10.0
 
-srun julia --project=/home/acrespi/scratch/Flinch -t 64 /home/acrespi/scratch/Flinch/src/PATHinit.jl
+srun julia --project=/home/acrespi/scratch/Flinch -t 32 /home/acrespi/scratch/Flinch/src/PATHinit.jl
